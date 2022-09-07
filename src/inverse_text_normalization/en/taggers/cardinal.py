9ae5b3a21d8ee33e7c4eadf2d75bd0c76d97386e
Jeevesh88 <113009521+Jeevesh88@users.noreply.github.com>
2022-09-07 22:59:45 +0530

    Added Docs

--- src/inverse_text_normalization/en/taggers/cardinal.py
+++ src/inverse_text_normalization/en/taggers/cardinal.py
@@ -43,12 +43,12 @@ class CardinalFst(GraphFst):
     def __init__(self):
         super().__init__(name="cardinal", kind="classify")
         # integer, negative
-
+        
         graph_zero = pynini.string_file(get_abs_path("data/numbers/zero.tsv"))
         graph_digit = pynini.string_file(get_abs_path("data/numbers/digit.tsv"))
         graph_ties = pynini.string_file(get_abs_path("data/numbers/ties.tsv"))
-        graph_teen = pynini.string_file(get_abs_path("data/numbers/teen.tsv"))
-
+        graph_teen = pynini.string_file(get_abs_path("data/numbers/teen.tsv")) #.string_file() creates a transducer to map between cols from a tsv
+        
         graph_hundred = pynini.cross("hundred", "")
 
         graph_hundred_component = pynini.union(graph_digit + delete_space + graph_hundred, pynutil.insert("0"))
